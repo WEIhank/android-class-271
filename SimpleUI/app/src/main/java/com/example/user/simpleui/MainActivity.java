a53b9eb7f5fc8fe7f9a59cabd3c3ed982d61bdce
FrankLikuohao <frank.likuohao@gmail.com>
2016-07-26 11:36:49 +0800

    add bug

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,21 +3,50 @@ package com.example.user.simpleui;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
         TextView textView;
+        EditText editText;
+        RadioGroup radioGroup;
+
+        String sex = "male";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    textView = (TextView)findViewById(R.id.textView);
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
 
-    }
-    public void submit(View view)
-    {
-        textView.setText("Hellow android");
-    }
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-    }
+            }
+        }
+
+    public void onCheckedChanged(RadioGroup group, int checkedId)() {
+                if (checkedId == R.id.maleRadioButton) {
+                    sex = "male";
+                } else if (checkedId == R.id.femaleRadioButton) {
+                    sex = "female";
+                }
+            });
+
+            public void submit(View view) {
+                String text = editText.getText().toString();
+                text = text + "sex:" + sex;
+                textView.setText(text);
+                editText.setText("");
+
+            }
+        }
+
+
+
+
+}
